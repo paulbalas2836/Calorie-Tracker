dc6f73015b6abf32e5824c5f4e8d883ca4060c85
Paul Balas <paulbalas2836@yahoo.com>
2022-04-26 17:33:57 +0300

    Canvas home page

--- src/main/web/src/pages/Home.vue
+++ src/main/web/src/pages/Home.vue
@@ -3,9 +3,6 @@
     <div class="absolute z-10 lg:text-6xl text-4xl font-extrabold dark:text-white mt-10" ref="titleText">Artificial FOOD</div>
     <div class="absolute z-10 lg:text-7xl text-5xl font-extrabold mt-28 dark:text-white" ref="bigText">CHECK CALORIES FAST</div>
     <canvas class="min-h-screen w-full absolute z-10" ref="canvas" ></canvas>
-<!--    <img src="/CerealImage.png" alt="" ref="cerealImage" class="absolute z-10">-->
-<!--    <img src="/leftFoodsImage.png" alt="" ref="leftFoodsImage" class="absolute z-10">-->
-<!--    <img src="/rightFoodsImage.png" alt="" ref="rightFoodsImage" class="absolute z-10">-->
   </div>
 </template>
 
@@ -15,19 +12,37 @@ import {onMounted, onUnmounted, ref, watch} from 'vue'
 
 const canvas = ref(null);
 const savedContext= ref(null);
+const mouse = ref({x:null, y:null})
 const titleText = ref(null);
 const bigText = ref(null);
 const normalText = ref(null);
 
+function getMousePosition(event){
+  mouse.value.x = event.clientX
+  mouse.value.y = event.clientY
+}
 
 
 onMounted(()=>{
+  const leftImageXPosition = 0;
+  const leftImageYPosition = 50;
+
+  const rightImageXPosition = 0;
+  const rightImageYPosition = 50;
+
+  let middleImageXPosition = null;
+  let middleImageYPosition = null;
+
   let cssWidth = null;
   let cssHeight = null;
   let dpr = null;
   let pxWidth = null;
   let pxHeight = null;
-  const leftImage = new Image()
+  let context = null;
+  let animation = null;
+  const leftImage = new Image();
+  const rightImage = new Image();
+  const middleImage = new Image();
 
   function redraw() {
     dpr = window.devicePixelRatio
@@ -37,39 +52,62 @@ onMounted(()=>{
     pxHeight = Math.round(dpr * cssHeight)
     canvas.value.width = pxWidth
     canvas.value.height = pxHeight
-    const context = canvas.value.getContext("2d")
+    context = canvas.value.getContext("2d")
     context.scale(dpr, dpr)
     savedContext.value = context
 
-    leftImage.src = '/leftFoodsImage.png'
+    middleImageXPosition = 0;
+    middleImageYPosition = 50;
+    leftImage.src = '/leftFoodsImage.png';
     leftImage.onload = function () {
-      context.drawImage(leftImage, 0, 50, cssWidth, cssHeight);
+      context.drawImage(leftImage, leftImageXPosition, leftImageYPosition, cssWidth, cssHeight);
+    }
+
+    rightImage.src='/rightFoodsImage.png';
+    rightImage.onload = function() {
+      context.drawImage(rightImage, rightImageXPosition, rightImageYPosition, cssWidth, cssHeight)
+    }
+
+    middleImage.src = '/CerealImage.png'
+    middleImage.onload = function (){
+      context.drawImage(middleImage, middleImageXPosition, middleImageYPosition, cssWidth,cssHeight)
     }
   }
-  new ResizeObserver(() => redraw()).observe(canvas.value)
+  redraw();
+  // const resizeObserver = new ResizeObserver(() => redraw())
+  // resizeObserver.observe(canvas.value)
 
-  function parallax(e) {
-    let bowlImageX = (e.clientX * 3) / 250;
-    let bowlImageY = (e.clientY * 2) / 250;
+  function parallax() {
+    if (animation) {
+      window.cancelAnimationFrame(animation)
+    }
+    animation = requestAnimationFrame(parallax)
+    let bowlImageX = (mouse.value.x * 4) / 250;
+    let bowlImageY = (mouse.value.y * 4) / 250;
 
-    let rightImageX = (e.clientX * 2) / 250;
-    let rightImageY = (e.clientY * 3) / 250;
+    let rightImageX = (mouse.value.x * 2) / 250;
+    let rightImageY = (mouse.value.y * 2) / 250;
 
-
-    
-    rightFoodsImage.value.style.transform = "translateX(" + rightImageX + "px) translateY(" + rightImageY + "px)";
-    leftFoodsImage.value.style.transform = "translateY(" + bowlImageY + "px)";
+    context.clearRect(0, 0, cssWidth, cssHeight);
+    context.drawImage(leftImage,leftImageXPosition,leftImageYPosition + rightImageY, cssWidth, cssHeight);
+    context.drawImage(rightImage,rightImageXPosition + rightImageX,rightImageYPosition + rightImageY, cssWidth, cssHeight);
+    context.drawImage(middleImage,middleImageXPosition + bowlImageX,middleImageYPosition + bowlImageY, cssWidth, cssHeight);
+    //
+    // rightFoodsImage.value.style.transform = "translateX(" + rightImageX + "px) translateY(" + rightImageY + "px)";
+    // leftFoodsImage.value.style.transform = "translateY(" + bowlImageY + "px)";
 
     // titleText.value.style.transform = "translateX(" + (e.clientX * 2) / 250 + "px) translateY(" + e.clientY / 250 + "px)";
     // bigText.value.style.transform = "translateX(" + (e.clientX * 2) / 250 + "px) translateY(" + e.clientY / 250 + "px)";
     // normalText.value.style.transform = "translateX(" + (e.clientX * 2) / 250 + "px) translateY(" + e.clientY / 250 + "px)";
 
   }
+  parallax()
 
-  addEventListener("mousemove", parallax);
+  addEventListener("mousemove", getMousePosition);
 
   onUnmounted(() => {
-    removeEventListener("mousemove", parallax);
+    removeEventListener("mousemove", getMousePosition);
+    // resizeObserver.disconnect()
   })
 })
 
--- src/main/web/src/pages/MainPage.vue
+++ src/main/web/src/pages/MainPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-full w-full">
-    <canvas class="min-h-screen w-full absolute z-0" ref="canvas"></canvas>
+    <div class="bg-light-home min-h-screen w-full absolute z-0" v-show="homePage"></div>
     <navbar></navbar>
     <main>
       <router-view></router-view>
@@ -11,43 +11,14 @@
 
 <script setup>
 import Navbar from "../components/navbar/Navbar.vue"
-import {onMounted, ref, watch} from "vue";
+import {ref, watch} from "vue";
 import {useRoute} from "vue-router";
 
-const canvas = ref(null)
 const homePage = ref(false)
 
 const currentRouteName = useRoute();
 watch(currentRouteName, () => {
   homePage.value = currentRouteName.name === 'Home';
 })
-
-
-onMounted(() => {
-  let cssWidth = null;
-  let cssHeight = null;
-  let dpr = null;
-  let pxWidth = null;
-  let pxHeight = null;
-
-  function redraw() {
-    dpr = window.devicePixelRatio
-    cssWidth = canvas.value.clientWidth
-    cssHeight = canvas.value.clientHeight
-    pxWidth = Math.round(dpr * cssWidth)
-    pxHeight = Math.round(dpr * cssHeight)
-    canvas.value.width = pxWidth
-    canvas.value.height = pxHeight
-    const context = canvas.value.getContext("2d")
-    context.scale(dpr, dpr)
-    const backgroundImage = new Image()
-    backgroundImage.src = '/backgroundImage.png'
-    backgroundImage.onload = function () {
-      context.drawImage(backgroundImage, 0, 0, cssWidth, cssHeight);
-    }
-  }
-  new ResizeObserver(() => redraw()).observe(canvas.value)
-})
-
 
 </script>
