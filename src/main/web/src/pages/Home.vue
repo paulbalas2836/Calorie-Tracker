34015d80c789882e749b2e311b95bd34976009d5
Paul Balas <paulbalas2836@yahoo.com>
2022-04-28 14:00:16 +0300

    animation

--- src/main/web/src/pages/Home.vue
+++ src/main/web/src/pages/Home.vue
@@ -1,37 +1,47 @@
 <template>
-  <div class="overflow-hidden flex justify-center">
-    <div class="absolute z-10 lg:text-6xl text-4xl font-extrabold dark:text-white mt-10" ref="titleText">Artificial FOOD</div>
-    <div class="absolute z-10 lg:text-7xl text-5xl font-extrabold mt-28 dark:text-white" ref="bigText">CHECK CALORIES FAST</div>
-    <canvas class="min-h-screen w-full absolute z-10" ref="canvas" ></canvas>
+  <div v-show="scrollUpOpacityText >= 0" class="flex justify-center">
+    <div class="absolute z-10 lg:text-6xl text-4xl font-extrabold dark:text-white mt-10" ref="titleText">Artificial
+      FOOD
+    </div>
+    <div class="absolute z-10 lg:text-7xl text-5xl font-extrabold mt-28 dark:text-white" ref="subtitleText">CHECK
+      CALORIES FAST
+    </div>
   </div>
+  <canvas class="min-h-screen w-full absolute " ref="canvas"></canvas>
 </template>
 
 <script setup>
 import {darkMode} from "../SealConstants";
 import {onMounted, onUnmounted, ref, watch} from 'vue'
 import Navbar from "../components/navbar/Navbar.vue"
-const canvas = ref(null);
-const savedContext= ref(null);
-const mouse = ref({x:null, y:null})
-const titleText = ref(null);
-const bigText = ref(null);
-const normalText = ref(null);
 
-function getMousePosition(event){
+const canvas = ref(null);
+const savedContext = ref(null);
+const mouse = ref({x: null, y: null})
+const titleText = ref(null);
+const subtitleText = ref(null);
+const scrollUpOpacityText = ref(1);
+
+function getMousePosition(event) {
   mouse.value.x = event.clientX
   mouse.value.y = event.clientY
 }
 
 
-onMounted(()=>{
-  const leftImageXPosition = 0;
-  const leftImageYPosition = 50;
+onMounted(() => {
+  let leftImageXPosition = 0;
+  let leftImageYPosition = 100;
 
-  const rightImageXPosition = 0;
-  const rightImageYPosition = 50;
+  let rightImageXPosition = 0;
+  let rightImageYPosition = 100;
 
-  let middleImageXPosition = null;
-  let middleImageYPosition = null;
+  let middleImageXPosition = 0;
+  let middleImageYPosition = 100;
+
+  let scrollUpTranslateTextY = 0;
+
+  let scrollDownTranslateTextY = 0;
+  let scrollDownOpacityText = 0;
 
   let cssWidth = null;
   let cssHeight = null;
@@ -57,31 +67,30 @@ onMounted(()=>{
     context.scale(dpr, dpr)
     savedContext.value = context
 
-    middleImageXPosition = 0;
-    middleImageYPosition = 50;
     leftImage.src = '/leftFoodsImage.png';
     leftImage.onload = function () {
       context.drawImage(leftImage, leftImageXPosition, leftImageYPosition, cssWidth, cssHeight);
     }
 
-    rightImage.src='/rightFoodsImage.png';
-    rightImage.onload = function() {
-      context.drawImage(rightImage, rightImageXPosition, rightImageYPosition, cssWidth, cssHeight)
+    rightImage.src = '/rightFoodsImage.png';
+    rightImage.onload = function () {
+      context.drawImage(rightImage, rightImageXPosition, rightImageYPosition, cssWidth, cssHeight);
     }
 
-    middleImage.src = '/CerealImage.png'
-    middleImage.onload = function (){
-      context.drawImage(middleImage, middleImageXPosition, middleImageYPosition, cssWidth,cssHeight)
+    middleImage.src = '/CerealImage.png';
+    middleImage.onload = function () {
+      context.drawImage(middleImage, middleImageXPosition, middleImageYPosition, cssWidth, cssHeight);
     }
   }
+
   redraw();
   // const resizeObserver = new ResizeObserver(() => redraw())
   // resizeObserver.observe(canvas.value)
 
-  function checkScroll(event){
-    if(event.wheelDelta >= 0 && scrollType === "steady")
+  function checkScroll(event) {
+    if (event.wheelDelta >= 0 && scrollType === "steady")
       scrollType = "up"
-    if(event.wheelDelta <= 0 && scrollType === "steady")
+    if (event.wheelDelta <= 0 && scrollType === "steady")
       scrollType = "down"
   }
 
@@ -96,21 +105,40 @@ onMounted(()=>{
 
     let rightImageX = (mouse.value.x * 2) / 250;
     let rightImageY = (mouse.value.y * 2) / 250;
+
+
     context.clearRect(0, 0, cssWidth, cssHeight);
-    context.drawImage(leftImage,leftImageXPosition,leftImageYPosition + rightImageY, cssWidth, cssHeight);
-    context.drawImage(rightImage,rightImageXPosition + rightImageX,rightImageYPosition + rightImageY, cssWidth, cssHeight);
-    context.drawImage(middleImage,middleImageXPosition + bowlImageX,middleImageYPosition + bowlImageY, cssWidth, cssHeight);
+    context.drawImage(leftImage, leftImageXPosition, leftImageYPosition + rightImageY, cssWidth, cssHeight);
+    context.drawImage(rightImage, rightImageXPosition + rightImageX, rightImageYPosition + rightImageY, cssWidth, cssHeight);
+    context.drawImage(middleImage, middleImageXPosition + bowlImageX, middleImageYPosition + bowlImageY, cssWidth, cssHeight);
 
-    if(scrollType === "up" && middleImageYPosition !== -600){
-      middleImageYPosition -= 5;
+    if (scrollType === "down" && middleImageYPosition !== -600) {
+      middleImageYPosition -= 10;
+      rightImageXPosition += 10;
+      leftImageXPosition -= 10;
+      scrollUpTranslateTextY--;
+      scrollUpOpacityText.value -= 0.02;
+      titleText.value.style.transform = "translateY(" + scrollUpTranslateTextY + "px)";
+      titleText.value.style.opacity = scrollUpOpacityText.value;
+      subtitleText.value.style.transform = "translateY(" + scrollUpTranslateTextY + "px)";
+      subtitleText.value.style.opacity = scrollUpOpacityText.value;
     }
 
-    if(scrollType === "down" && middleImageYPosition !== 50){
-      middleImageYPosition += 5;
+    if (scrollType === "up" && middleImageYPosition !== 50) {
+      middleImageYPosition += 10;
+      rightImageXPosition -= 10;
+      leftImageXPosition += 10;
+      scrollUpTranslateTextY++;
+      scrollUpOpacityText.value += 0.02;
+      titleText.value.style.transform = "translateY(" + scrollUpTranslateTextY + "px)";
+      titleText.value.style.opacity = scrollUpOpacityText.value;
+      subtitleText.value.style.transform = "translateY(" + scrollUpTranslateTextY + "px)";
+      subtitleText.value.style.opacity = scrollUpOpacityText.value;
     }
 
-    if(middleImageYPosition === -600 || middleImageYPosition === 50)
-    scrollType = "steady";
+    if (middleImageYPosition === -600 || middleImageYPosition === 50) {
+      scrollType = "steady";
+    }
 
     //
     // rightFoodsImage.value.style.transform = "translateX(" + rightImageX + "px) translateY(" + rightImageY + "px)";
@@ -121,6 +149,7 @@ onMounted(()=>{
     // normalText.value.style.transform = "translateX(" + (e.clientX * 2) / 250 + "px) translateY(" + e.clientY / 250 + "px)";
 
   }
+
   parallax()
 
   window.addEventListener('mousewheel', checkScroll);
@@ -131,7 +160,6 @@ onMounted(()=>{
     // resizeObserver.disconnect()
   })
 })
-
 
 
 </script>
